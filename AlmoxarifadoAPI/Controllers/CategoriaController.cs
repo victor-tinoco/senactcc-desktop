fc62f80811a2555d29816cc1d3c86c8f0accb1c8
gabriel yago <gabriellok1212@gmail.Com>
2019-11-11 20:35:34 -0300

    Categoria

---
+++ AlmoxarifadoAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,35 @@
+﻿using AlmoxarifadoSenacLib.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace AlmoxarifadoAPI.Controllers
+{
+    public class CategoriaController : ApiController
+    {
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [Authorize]
+        public IHttpActionResult Get()
+        {
+            var filtro = "";
+            try
+            {
+
+                CategoriaRepositorio repo = new CategoriaRepositorio();
+                var Categoria = repo.PesquisarCategoria(filtro);
+                return Ok(Categoria);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+
+            }
+
+
+        }
+    }
+}
